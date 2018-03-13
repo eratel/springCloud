0ea41865949bb16970fc907bfb6ad87db7f97b15
jikai.sun <425359983@qq.com>
2018-03-13 21:00:54 +0800

    版本更新

--- movie-fegin-hystrix/src/main/java/com/example/demo/MovieFeginHystrixApplication.java
+++ movie-fegin-hystrix/src/main/java/com/example/demo/MovieFeginHystrixApplication.java
@@ -10,15 +10,15 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  * @params: No such property: code for class: Script1
  * @Author:jikai.sun
  * @Date: Created in 23:53 2017/10/15 0015
- * @Description: hystrix 断路器   某个节点单独使用短路器  看conf package
- *
+ * @Description: hystrix 断路器   某个节点单独使用短路器  看Configuration2
+ * Configuration2  去除了对hystrix的支持
  */
 @SpringBootApplication
 @EnableEurekaClient
 //Feign
 @EnableFeignClients
 //hystrix
-// UserFeignClient 实现了对hystrix的支持
+// UserFeignClient 实现了对hystrixDashBoard的支持
 @EnableCircuitBreaker
 public class MovieFeginHystrixApplication
 {
--- movie-hystrix-dashboard/src/main/java/com/example/MovieHystrixApplication.java
+++ movie-hystrix-dashboard/src/main/java/com/example/MovieHystrixApplication.java
@@ -1,13 +1,14 @@
-package com.itmuch.cloud;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableHystrixDashboard
 @SpringBootApplication
-public class EurekaApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(EurekaApplication.class, args);
-  }
+@EnableHystrixDashboard
+public class MovieHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHystrixApplication.class, args);
+	}
 }
