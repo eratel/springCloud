9f43f08b21038cf9918de7764aaea180413bd214
jikai.sun <425359983@qq.com>
2018-03-20 20:57:52 +0800

    add zuul hystrix rallback

---
+++ gateway-zuul-rallback/src/main/java/com/example/demo/ZuulRallbackApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulRallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulRallbackApplication.class, args);
+	}
+
+}
